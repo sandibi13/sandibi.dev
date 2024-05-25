aa252dd79d26634be242758c669fa1c9bf7309dc
sandibi13 <sandipanb223@gmail.com>
2024-05-25 12:35:35 +0530

    added view count with upstash/redis

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx()]
+  integrations: [react(), tailwind(), mdx()],
 });
--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,3 +1,5 @@
+import { Eye } from "lucide-react";
+
 interface Project {
   date?: string;
   title: string;
@@ -6,9 +8,10 @@ interface Project {
 
 interface ArticleProps {
   project: Project;
+  views: number;
 }
 
-export const Article: React.FC<ArticleProps> = ({ project }) => {
+export const Article: React.FC<ArticleProps> = ({ project, views }) => {
   return (
     <article className="p-4 md:p-8">
       <div className="flex justify-between gap-2 items-center">
@@ -23,11 +26,15 @@ export const Article: React.FC<ArticleProps> = ({ project }) => {
             <span>SOON</span>
           )}
         </span>
+        <span className="text-zinc-500 text-xs flex items-center gap-1">
+          <Eye className="w-4 h-4" />{" "}
+          {Intl.NumberFormat("en-US", { notation: "compact" }).format(views)}
+        </span>
       </div>
       <h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
         {project.title}
       </h2>
-      <p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
+      <p className="z-20 mt-4 text-sm duration-1000 text-zinc-400 group-hover:text-zinc-200">
         {project.description}
       </p>
     </article>
