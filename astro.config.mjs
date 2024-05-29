3f280e8efd1c4126d0c13999ca8789ae30d27b40
sandibi13 <sandipanb223@gmail.com>
2024-05-29 16:21:56 +0530

    fixed view counter increment issue and removed astro's view-transition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
 import dotenv from "dotenv";
-
+import vercel from "@astrojs/vercel/serverless";
 dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), mdx()],
+  output: "server",
+  adapter: vercel(),
 });
